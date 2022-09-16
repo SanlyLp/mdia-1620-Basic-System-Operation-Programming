94548ece153c594fc45b17ff23c1e74393c57d09
sanly Lotfzad Pak <slotfzadpak@my.bcit.ca>
2022-09-16 12:17:08 -0700

    adding more code

--- part-1/week1/example.js
+++ part-1/week1/example.js
@@ -1,4 +1,39 @@
-"testing" 
-// Declare a variable called car
+//camelCase
 
-var car;
+var bugsCollected = 4;
+
+//PascalCase
+var BugsCollected =4;
+
+//SnakeCase
+
+var Bugs_collected =4;
+
+var electronicsBox = 'cpu';
+
+//boolean
+//initialise to the value true!
+var proceed = true;
+var i = 0;
+while (proceed) {
+    i++;
+    proceed = false;
+}
+console.log(i);
+
+// Data type -object
+
+/**
+ * create a variable called grannySmith
+ * color is green
+ * origin is australia
+ * origin date is 1868
+ * then console log the color from object
+ */
+
+var grannySmith = {
+    color: 'green',
+    originLocation:'australia',
+    originDate:1868};
+
+console.log(grannySmith);
